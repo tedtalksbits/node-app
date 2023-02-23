053fe20287a80ab99a9250a70885c11f5cf20292
Tedane Blake <tblake@ppvglobal.com>
2023-02-23 12:23:59 -0500

    update port

--- index.js
+++ index.js
@@ -29,6 +29,6 @@ app.get('/:pokemon', (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(8080, () => {
+    console.log('Server is running on port 8080');
 });
