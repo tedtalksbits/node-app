4653e6cd93edfa1174e00ddb4797b2182327f2d2
Tedane Blake <tblake@ppvglobal.com>
2023-02-23 12:08:41 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# This is basic example of an Express app
+
+## About the app
+
+**Routes**
+    
+        / - Home page
+        /:pokemon
+            - Get pokemon data
+            - example: /pikachu - returns pikachu data
+
+
+## File structure
+
+    .
+    ├── README.md
+    ├── index.js
+    ├── package.json
+    ├── package-lock.json
+
+
+## Installation
+    
+        `npm install`
+
+
+## Run the app
+
+        `npm run dev`
+
+## Run the app in production mode
+    
+            `npm start`
+
---
+++ index.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/:pokemon', (req, res) => {
+    const pokemons = [
+        { name: 'Pikachu', type: 'electric' },
+        { name: 'Bulbasaur', type: 'grass' },
+        { name: 'Charmander', type: 'fire' },
+        { name: 'Squirtle', type: 'water' },
+    ];
+
+    const requestedPokemon = req.params.pokemon;
+    const pokemon = pokemons.find((pokemon) => pokemon.name.toLowerCase() === requestedPokemon.toLowerCase());
+
+    if (pokemon) {
+        res.json(pokemon);
+    } else {
+        res.status(404).send('Pokemon not found');
+    }
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
