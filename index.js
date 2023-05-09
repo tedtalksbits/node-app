4ef9e6b0a4ce486c3372aa35023675890035c8dd
tedtalksbits <tedaneblake@gmail.com>
2023-05-08 23:21:45 -0400

    remove app config

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ import { pokemon } from './data/pokemon.js';
 import { createDocument } from './lib/createDocument.js';
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(cors());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
